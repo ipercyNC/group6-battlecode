cae444818a194c0140fd6aa0619feaa91062d3d5
resheet <resheetschultz@gmail.com>
2019-01-28 12:27:14 -0800

    Added movement function.

--- test/nextMoveToDestination.test.js
+++ test/nextMoveToDestination.test.js
@@ -1,7 +1,36 @@
 import nextMoveToDestination from "../bots/nextMoveTodestination.js";
 
+const makeTerrain = () => {
+  return [
+  // 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 0
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 1
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 2
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 3
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 4
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 5
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 6
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 7
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 8
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 9
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 10
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 11
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 12
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 13
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 14
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]; // 15
+};
+
 describe("function nextMoveToDestination", () => {
-  it("should return Types.FILTER_NOTHING if the hit requester id does not match any entry's requester id", () => {
-    nextMoveToDestination();
+  it("should move to the best empty square in range", () => {
+    const moveLocation1 = nextMoveToDestination(9, 9, 1, 0, 3, makeTerrain());
+
+    expect(moveLocation1).toEqual([7, 7]);
+  });
+  it("should not move into an occupied square", () => {
+    const terrain = makeTerrain();
+    terrain[7][7] = 0;
+    const moveLocation1 = nextMoveToDestination(9, 9, 1, 0, 3, terrain);
+    expect(moveLocation1).toEqual([8, 7]);
   });
 });
