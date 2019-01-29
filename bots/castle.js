774ad7f489eaa1c147462c261f4f675644672e7f
Unknown <letatanu@gmail.com>
2019-01-29 05:14:03 -0800

    Adding castle's attack

--- bots/castle.js
+++ bots/castle.js
@@ -3,12 +3,41 @@ import { BCAbstractRobot, SPECS } from "battlecode";
 const castle = {};
 
 castle.takeTurn = (self) => {
+  // get visible robots
+  const visible = self.getVisibleRobots();
+
+  // get attackable robots
+  const attackable = visible.filter((r) => {
+    if (!self.isVisible()) {return false;}
+    const dist = (self.me.x - r.x) * (self.me.x - r.x) + (self.me.y - r.y) * (self.me.y - r.y);
+    if (self.me.team !== r.team &&
+    SPECS.UNITS[self.me.unit].ATTACK_RADIUS[0] <= dist
+  && dist <= SPECS.UNITS[self.me.unit].ATTACK_RADIUS[1]) { return true; }
+    return false;
+  });
+
+
   // self.log("castle taking turn");
   if (self.step % 100) {
     // self.log('KNOWN ENEMY CASTLES: ');
     for (let i = 0; i < self.enemyCastles.length; i++) {
       const { x, y } = self.enemyCastles[i];
       self.log(x + "," + y);
+    }
+  }
+
+  // Attack the first robot if it is Prophet
+  if (attackable.length > 0) {
+    for (const x in attackable) {
+      if (x.unit === SPECS.PROPHET && self.fuel >= 50) {
+        return self.attack(x.x - self.me.x, x.y - self.me.y);
+      }
+    }
+    // get the first robot
+    const r = attackable[0];
+    // attack the first robot if the fuel is higher than 60
+    if (self.fuel >= 60) {
+      return self.attack(r.x - self.me.x, r.y - self.me.y);
     }
   }
 
